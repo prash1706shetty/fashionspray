ca16a9bbbee9a13f3bbb7e2d9e118dd6c11d13f3
Prashantha Shetty <pshetty9@in.ibm.com>
2021-03-19 18:43:48 +0530

    deleted

--- models/cloudant.js
+++ models/cloudant.js
@@ -12,19 +12,7 @@ var data = {
     "services": {
         "cloudantNoSQLDB": {
             "credentials": {
-                //credentials
-                "apikey": "vDA8DBVCP-r3aTu5e7rlizOIyO6zMgBdyFV7HitZlutq",
-                "host": "90ea8a86-a890-4beb-8c1a-4b6e6ed167c9-bluemix.cloudantnosqldb.appdomain.cloud",
-                "iam_apikey_description": "Auto-generated for key 1ae267af-8a43-4c26-b7b2-55981bd60b8b",
-                "iam_apikey_name": "fashionspray-connection",
-                "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
-                "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/4f76577546644bfcae24bd93313bb1ab::serviceid:ServiceId-e9d1c2a8-0ec9-4bb5-88c6-2709c2372106",
-                "password": "4e31bf1c16ee4d536cdfc16f7ce66e7dbc5afa714ab9f593d30595678ed19db9",
-                "port": 443,
-                "url": "https://90ea8a86-a890-4beb-8c1a-4b6e6ed167c9-bluemix:4e31bf1c16ee4d536cdfc16f7ce66e7dbc5afa714ab9f593d30595678ed19db9@90ea8a86-a890-4beb-8c1a-4b6e6ed167c9-bluemix.cloudantnosqldb.appdomain.cloud",
-                "username": "90ea8a86-a890-4beb-8c1a-4b6e6ed167c9-bluemix"
-              },
-            "label": "cloudantNoSQLDB"
+
         }
     }
 }
