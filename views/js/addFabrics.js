023dbc129ad1fa6755b91949f158a524ccbbb8a1
Prashantha Shetty <pshetty9@in.ibm.com>
2021-04-14 16:43:48 +0530

    add fabrics changes

--- views/js/addFabrics.js
+++ views/js/addFabrics.js
@@ -1,6 +1,32 @@
 var formValidation = true;
+var addCount = 1;
 
 jQuery(document).ready(function ($) {
+
+    var searchParams = new URLSearchParams(window.location.search);
+    let param = searchParams.get('on')
+    var doc = {
+        docId: 'fs21200'
+    }
+
+    jQuery.ajax({
+        type: "POST",
+        url: "/getOrder",
+        data: JSON.stringify(doc),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        async: false,
+        success: function (result) {
+
+            preloadForm(result.data);
+        },
+        error: function (e) {
+            alert("There was some internal error while updating, Please try again after sometime")
+        }
+    });
+
+
     $("#fabricsFrom").change(function () {
         var faricsFrom = $('#fabricsFrom').val();
         if (faricsFrom == 'fs') {
@@ -10,11 +36,10 @@ jQuery(document).ready(function ($) {
         }
 
     });
-    var addCount = 1;
     $("#Add").on("click", function () {
         addCount++;
         $("#textboxDiv").append(' <div class=" ibm-col-12-12"><div class=" ibm-col-12-3">' +
-            '<div for="fabricType' + addCount + '" class="ibm-textcolor-green-60 ibm-padding-top-1 tooltip">Fabric type' +
+            '<div for="fabricType' + addCount + '" class="ibm-textcolor-green-60 ibm-padding-top-1 tooltip">Fabric type ' + addCount +
             '<span class="tooltiptext">Type of fabric.</span>' +
             '<span class="ibm-required">' +
             '*</span>' +
@@ -49,13 +74,13 @@ jQuery(document).ready(function ($) {
             '</div>' +
 
             '<div class=" ibm-col-12-3">' +
-            '<div for="fabricNote' + addCount + '" class="ibm-textcolor-green-60 ibm-padding-top-1 tooltip">Note and description of fabric.' +
+            '<div for="fabricNote' + addCount + '" class="ibm-textcolor-green-60 ibm-padding-top-1 tooltip">Note' +
             '<span class="tooltiptext">Note.</span>' +
             '<span class="ibm-required">' +
             '*</span>' +
             '</div>' +
             '<div class="fieldPaddingTop">' +
-            '<textarea placeholder="Enter fabric details." value="" rows="1" class="fabricNote" id="fabricNote' + addCount + '"' +
+            '<textarea placeholder="Enter fabric details." value="" rows="2" class="fabricNote" id="fabricNote' + addCount + '"' +
             'name="orderNote' + addCount + '" maxlength="500"></textarea>' +
             '</div>' +
             '</div>' +
@@ -78,6 +103,20 @@ jQuery(document).ready(function ($) {
         }
     }, 100); // check every 100ms
 });
+
+function preloadForm(result) {
+    jQuery('#customerName').text(result.customerName);
+    jQuery('#mobileNumber').text(result.mobileNumber);
+    jQuery('#orderNumber').text(result.orderNumber);
+    jQuery('#dress').text(result.dressFor + ' ' + result.dressType);
+    jQuery('#orderDate').text(result.orderDate.date + '/' + result.orderDate.month + '/' + result.orderDate.year);
+    jQuery('#deliveryDate').text(result.deliveryDate.date + '/' + result.deliveryDate.month + '/' + result.deliveryDate.year);
+    jQuery('#orderNote').text(result.orderNote);
+}
+
+function saveOrder() {
+    console.log("count-->" + addCount);
+}
 
 function clearRedColor(focusEvent) {
     jQuery('#' + focusEvent.id).removeClass('redBorder');
