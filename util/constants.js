c7c6ed014dded2046b43c455a269395a194e9f07
Prashantha Shetty <pshetty9@in.ibm.com>
2021-05-11 13:08:04 +0530

    constants file deleted

--- util/constants.js
+++
@@ -1,3 +0,0 @@
-module.exports.UNAUTHORIZED_USER_ERROR_MESSAGE = { error: { name: 'UserNotAuthorized', message: 'User is not authorized' } };
-module.exports.UNAUTHENTICATED_USER_ERROR_MESSAGE = { error: { name: 'UserNotAuthenticated', message: 'User is not authenticated' } };
-module.exports.USERLOGGEDOUT_USER_ERROR_MESSAGE = { error: { name: 'UserLoggedOut', message: 'User logged out, please login again' } };
